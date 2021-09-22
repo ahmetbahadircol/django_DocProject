d6c8bf135681425cc984d5b9a9003d7f91f9cedb
ahmetbahadircol <ahmetbahadircol@gmail.com>
2021-09-23 00:09:27 +0300

    SECRET_KEY is removed from setting.py and stored in .env

--- valid_document/settings.py
+++ valid_document/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_1h9saw@k70w^&y+b6l)q@-r9=o%ha0h*c3*z5eey9o9k+fh(z'
-
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
